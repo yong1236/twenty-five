885515d0ebe2c935049e1de5f8d4390a6bda9bd0
yong1236 <yong1236@foxmail.com>
2015-12-18 04:09:39 +0800

    init

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+grunt build
+cp -rf dist/scripts $HOME/workspace/eclipse/wp-tonyeen/wp-content/themes/twentyfive/
+cp -rf dist/styles $HOME/workspace/eclipse/wp-tonyeen/wp-content/themes/twentyfive/
